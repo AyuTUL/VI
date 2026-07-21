09d59e1fa1fe37dfb7afac1691eaaf9588c98b31
Ayush Tuladhar <165144922+AyuTUL@users.noreply.github.com>
2026-07-21 15:03:08 +0545

    Add files via upload

---
+++ CSC378-NET-Centric-Computing/Lab/1-class-object-creation/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+class Student
+{
+    public string Name { get; set; }
+    public int Roll { get; set; }
+    public float Marks { get; set; }
+
+    public Student(string name, int roll, float marks)
+    {
+        Name = name;
+        Roll = roll;
+        Marks = marks;
+    }
+    public void Display()
+    {
+        Console.WriteLine("\nStudent Details :\nName : {0}\nRoll : {1}\nMarks : {2}", Name, Roll, Marks);
+    }
+}
+
+class ClassObjectCreation
+{
+    static void Main()
+    {
+        Console.Write("Enter student details :\nEnter name : ");
+        string name = Console.ReadLine();
+
+        Console.Write("Enter roll : ");
+        int roll = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter marks : ");
+        float marks = float.Parse(Console.ReadLine());
+
+        Student s = new Student(name, roll, marks);
+
+        s.Display();
+    }
+}
